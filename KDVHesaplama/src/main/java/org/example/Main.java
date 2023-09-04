bafbc00ba61d74062a223543d2ddeda04a17695f
salihdurus <durussalih@gmail.com>
2023-09-05 00:33:48 +0300

    KDV hesaplama programı yapıldı.

---
+++ KDVHesaplama/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double tutar, kdv, kdvliTutar;
+        System.out.print("Tutarı giriniz : ");
+        tutar = input.nextDouble();
+        kdv = tutar > 1000 ? 0.18 : 0.08;
+        kdvliTutar = tutar + tutar * kdv;
+        System.out.println("KDV'siz Fiyat = " + tutar);
+        System.out.println("KDV'li Fiyat = " + kdvliTutar);
+        System.out.println("KDV tutarı = " + kdv);
+    }
+}
